60c38acb0e1e511aa115ba15284d197ef55d2405
Arjuna <a.james18@hotmail.com>
2017-06-08 09:14:00 -0500

    Guessing Game Unlimited

--- exercises3.py
+++ exercises3.py
@@ -26,20 +26,37 @@ for i in range(0,6):
 
 print u
 
-print u[random.randint(0,6)]
+print u[random.randint(0,5)]
 
 print " "
 print " "
 
 class GuessingGame:
     def __init__(self):
-        self.number = random.random()
+        self.number = random.randint(1,101)
+        win = False
+        self.getGuess()
 
     def num(self):
         print self.number
 
     def getGuess(self):
-        
+        self.guess = int(input("Choose an integer between 1 and 100."))
+        if self.guess < 1 or self.guess > 100:
+            print "Invalid entry."
+            self.getGuess()
+        else:
+            self.checkGuess()
 
-p = GuessingGame()
-p.num()
+    def checkGuess(self):
+            if self.guess < self.number:
+                print "Too low."
+                self.getGuess()
+            elif self.guess > self.number:
+                print "Too high."
+                self.getGuess()
+            else:
+                print "Well done. You guessed correctly."
+                win = True
+
+l = GuessingGame()
